8432e3020ea58506a3c902dcb3257eb4d1109f46
Timur Khayrutdinov <tmkhayrutdinov@gmail.com>
2018-08-08 23:47:16 +0300

    Create DNet_Hub project.

---
+++ DNetTool/DNet_Hub/Hubs/MainHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DNet_Hub.Hubs
+{
+    public class MainHub : Hub
+    {
+        public async Task OnRegister()
+        {
+            await Clients.AllExcept(Context.ConnectionId).SendAsync("");
+        }
+    }
+}
